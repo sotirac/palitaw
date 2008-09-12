0e7062aec2fd77e5c4c9e3d7aa954cac09ca40e6
sotirac <sotirac@gmail.com>
2008-09-12 00:33:39 -0400

    provided a check if there are less than 20 entries in a feed available

--- src/websites/views.py
+++ src/websites/views.py
@@ -9,18 +9,24 @@ def index(request):
 	
 	for x_website in websites:
 		# remove the old entries from the database
-		entries_to_delete = Entry.objects.filter(website=x_website)
-		for entry in entries_to_delete
-			entry.delete()
+		entries_to_delete = Entry.objects.filter(website=x_website)  
+		if entries_to_delete:
+			for entry_to_delete in entries_to_delete:
+				entry_to_delete.delete()   
 		
-		# get the new entries and save them to the database	
-		data = feedparser.parse(x_website.feed)
-		for i in range(0, 20):         
+		# get the new entries and save them to the database	    
+		# save a maximum of 20 entries into the database
+		data = feedparser.parse(x_website.feed)      
+		number_of_entries = len(data['entries']) 
+		entries_to_save = 20
+		if number_of_entries < 20:      
+			entries_to_save = number_of_entries
+		for i in range(0, entries_to_save):         
 			xEntry = data.entries[i]
 			xTitle = xEntry.title
 			xLink = xEntry.link  
-			entry = Entry(website=x_website, title=xTitle, url=xLink)
-			entry.save()
+			entry_to_save = Entry(website=x_website, title=xTitle, url=xLink)
+			entry_to_save.save()
 			
 	return render_to_response('websites/index.html', {'websites': websites})
 # index = cache_page(index, 60 * 60)
